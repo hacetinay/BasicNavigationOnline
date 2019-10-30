f9cbfbba803b9bfe43690b06ee8e603450f1494c
hacetinay <hacetinay@gmail.com>
2019-10-30 11:59:43 -0400

    second commit

--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -1,11 +1,21 @@
 package com.cbt.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserFactory {
     public static WebDriver getDriver(String  browser){
+        if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+return new ChromeDriver();
+        }else if(browser.equals("chrome")){
+WebDriverManager.firefoxdriver().setup();
+return new FirefoxDriver();
+        }
         return null;
-        String od= System.getProperty("os.name");
+
 
     }
 
